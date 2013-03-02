aeeb9a5f9b4a5efb97ed10b38a0c76a9febd614a
beefarino <jim.christopher@gmail.com>
2013-03-02 15:52:25 -0500

    new-item on windows configurations path will now explicitly apply the new configuration; this forces visual studio 2010+ to persist the window configuration between sessions.  incremented assembly version number in preparation of maintenance release 1.5.0.2

--- src/CodeOwls.StudioShell.AssemblyInfo.cs
+++ src/CodeOwls.StudioShell.AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyProduct("StudioShell - A Deeply Integrated PowerShell Module for Visual Studio Extensibility")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.2")]
+[assembly: AssemblyFileVersion("1.5.0.2")]

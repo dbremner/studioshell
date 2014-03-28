dc7728f139a74f74e756090a1c97f8d4dc9ca012
beefarino <jim.christopher@gmail.com>
2014-03-28 17:05:29 -0400

    fixed PS3/4 bug related to using custom provider with select-object

--- src/CodeOwls.StudioShell.AssemblyInfo.cs
+++ src/CodeOwls.StudioShell.AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyProduct("StudioShell - A Deeply Integrated PowerShell Module for Visual Studio Extensibility")]
 
-[assembly: AssemblyVersion("1.6.3.0")]
-[assembly: AssemblyFileVersion("1.6.3.0")]
+[assembly: AssemblyVersion("1.6.4.0")]
+[assembly: AssemblyFileVersion("1.6.4.0")]
 

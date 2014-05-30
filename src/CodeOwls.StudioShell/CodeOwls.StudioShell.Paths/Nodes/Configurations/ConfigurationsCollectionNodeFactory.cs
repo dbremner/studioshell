ca761f4e64598493e4f6c55fcf31ca2064502b92
beefarino <jim.christopher@gmail.com>
2014-05-30 16:10:26 -0400

    added configuration nodes

---
+++ src/CodeOwls.StudioShell/CodeOwls.StudioShell.Paths/Nodes/Configurations/ConfigurationsCollectionNodeFactory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeOwls.PowerShell.Provider.PathNodes;
+using EnvDTE;
+using EnvDTE80;
+
+namespace CodeOwls.StudioShell.Paths.Nodes
+{
+    public class PropertyNodeFactory : NodeFactoryBase
+    {
+        private readonly Property _property;
+
+        public PropertyNodeFactory(Property property)
+        {
+            _property = property;
+        }
+
+        public override IPathNode GetNodeValue()
+        {
+            return new PathNode( _property, Name, false );
+        }
+
+        public override string Name
+        {
+            get { return _property.Name; }
+        }
+    }
+    public class ConfigurationNodeFactory : NodeFactoryBase
+    {
+        private readonly Configuration _configuration;
+
+        public ConfigurationNodeFactory(Configuration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public override IEnumerable<INodeFactory> GetNodeChildren(PowerShell.Provider.PathNodeProcessors.IContext context)
+        {
+            List<INodeFactory> factories = new List<INodeFactory>();
+            foreach (Property p in _configuration.Properties)
+            {
+                var factory = new PropertyNodeFactory(p);
+                factories.Add(factory);
+            }
+            return factories;
+        }
+        public override IPathNode GetNodeValue()
+        {
+            return new PathNode( _configuration, Name, true );
+        }
+
+        public override string Name
+        {
+            get { return _configuration.ConfigurationName; }
+        }
+    }
+
+    public class ConfigurationsCollectionNodeFactory : CollectionNodeFactoryBase
+    {
+        private readonly Project _project;
+
+
+        public ConfigurationsCollectionNodeFactory( Project project )
+        {
+            _project = project;
+        }
+
+        public override IEnumerable<INodeFactory> GetNodeChildren(PowerShell.Provider.PathNodeProcessors.IContext context)
+        {
+            List<INodeFactory> factories = new List<INodeFactory>();
+            foreach (Configuration configuration in _project.ConfigurationManager)
+            {
+                var factory = new ConfigurationNodeFactory(configuration);
+                factories.Add( factory );
+            }
+            return factories;
+        }
+
+        public override string Name
+        {
+            get { return "Configurations"; }
+        }
+    }
+}
